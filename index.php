ae270d4840996752e9493a3682ac020698a5a294
malucario <122913162+malucario@users.noreply.github.com>
2024-11-20 16:39:31 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,12 @@ Include "view-header.php";
 ?>
 
 <h1>Learning JavaScript Functions!</h1>
- <p>Hello</p>
+ <p>Hello! Click the button below to get the time. If you'd like to see some other functions, travel to one of the other tabs.</p>
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.</button>
+
+<p id="demo"></p>
  <?php
 Include "view-footer.php";
 ?>
