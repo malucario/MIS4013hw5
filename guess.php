d367768f809e7b0607cca3398f63f58e12e7f799
malucario <122913162+malucario@users.noreply.github.com>
2024-11-20 19:37:30 -0600

    Update guess.php

--- guess.php
+++ guess.php
@@ -5,7 +5,7 @@ Include "view-header.php";
 <h1>Learning JavaScript Functions!</h1>
  <p>Hello! Let's play a guessing game! If you'd like to see some other functions, travel to one of the other tabs.</p>
 
-<p>Let's play a guessing game! I'm thinking of a number 1-10. Think you know what it is? Keep guessing until we think of the same number.</p>
+<p>Let's play a guessing game! I'm thinking of a number 1-10. Think you know what it is?</p>
   <input type="number" id="userGuess" min="1" max="10" placeholder="Your guess">
   <button onclick="checkGuess()">Submit Guess</button>
   <p id="result"></p>
