e6216566d507017fa6b0621be7751f4a498c93a9
malucario <122913162+malucario@users.noreply.github.com>
2024-11-20 19:07:49 -0600

    Update calc.php

--- calc.php
+++ calc.php
@@ -4,7 +4,7 @@ Include "view-header.php";
 ?>
 
 <h1>Learning JavaScript Functions!</h1>
- <p>Hello! Click the button below to get the time. If you'd like to see some other functions, travel to one of the other tabs.</p>
+ <p>Hello! Enter some numbers to do some simple math. If you'd like to see some other functions, travel to one of the other tabs.</p>
 <input type="number" id="num1" placeholder="Enter first number">
 <input type="number" id="num2" placeholder="Enter second number">
 <button class="button" onclick="calculateSum()">Calculate Sum</button>
