fd55e8ed9ddf4dbe599477f20df7a21e49fbcdaf
malucario <122913162+malucario@users.noreply.github.com>
2024-11-20 19:24:50 -0600

    Create guess.php

---
+++ guess.php
@@ -0,0 +1,11 @@
+<?php
+Include "view-header.php";
+?>
+
+<h1>Learning JavaScript Functions!</h1>
+ <p>Hello! Let's play a guessing game! If you'd like to see some other functions, travel to one of the other tabs.</p>
+
+
+<?php
+Include "view-footer.php";
+?>
