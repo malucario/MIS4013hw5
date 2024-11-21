850ae30d59f719c7516efdf7b6786f46ad67d5f5
malucario <122913162+malucario@users.noreply.github.com>
2024-11-20 19:20:08 -0600

    Update reverse.php

--- reverse.php
+++ reverse.php
@@ -1,4 +1,9 @@
+<?php
+Include "view-header.php";
+?>
 
+<h1>Learning JavaScript Functions!</h1>
+ <p>Hello! Enter something backwards and see it spat out forwards, or vice versa. If you'd like to see some other functions, travel to one of the other tabs.</p>
 
   <input type="text" id="textInput" placeholder="Enter text to reverse">
   <button class="button" onclick="reverseText()">Reverse Text</button>
@@ -11,3 +16,7 @@
       document.getElementById('reversedText').innerText = `Reversed Text: ${reversed}`;
     }
 </script>
+
+<?php
+Include "view-footer.php";
+?>
